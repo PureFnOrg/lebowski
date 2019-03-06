7f8de6c1792a5c9d1de807eea94accb25e305961
Matt Kocubinski <mkocubinski@gmail.com>
2019-03-06 16:35:08 -0500

    Version 2.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/lebowski "2.0.0"
+(defproject org.purefn/lebowski "2.0.1-SNAPSHOT"
   :description "A Couchbase implementation of the Bridges protocols."
   :url "https://github.com/PureFnOrg/lebowski"
   :license {:name "Eclipse Public License"
